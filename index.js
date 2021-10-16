a2a69759c1ec241727b553b8995d4e7dafad7b24
ezeilo-su <ezeilo.su@gmail.com>
2021-10-16 10:08:12 +0100

    Create event bus

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const morgan = require('morgan');
+const { json, urlencoded } = express;
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+
+app.post('/events', async (req, res) => {
+  const event = req.body;
+  await axios.post('http://localhost:4000/events', event);
+  await axios.post('http://localhost:4001/events', event);
+  await axios.post('http://localhost:4002/events', event);
+
+  res.status(200).json({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on port 4005');
+});
